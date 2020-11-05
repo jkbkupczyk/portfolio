7b46db8d5f85021753a32e4fcacc84c2d9fa0ce2
Jakub Kupczyk <69753899+jkbkupczyk@users.noreply.github.com>
2020-11-05 18:54:40 +0100

    Added to github pages

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,11 @@
-console.log("PHP is great");
+function sleep(x){
+    return new Promise((resolve) => setTimeout(resolve, x));
+}
+
+function foo(){
+    document.querySelector("#send").style.animation = "sweep 2s linear 1";
+
+    sleep(2050).then( () => {
+        document.querySelector("#form-info").innerHTML = "Email  sent!";
+    })
+}
